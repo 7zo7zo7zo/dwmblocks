b54b54a4d81e9c575a9497014465fffb38a6a509
7zo7zo7zo <7zo7zo7zo@gmail.com>
2022-01-25 23:04:32 -0800

    update

---
+++ blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
+        {"", "wifi_icon.sh",							5,              0},
+        {"", "vol_icon.sh",							5,              0},
+        {"", "battery_icon.sh",							5,              0},
+        {"", "date +'%a %b %d %Y %r'",						1,              0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;

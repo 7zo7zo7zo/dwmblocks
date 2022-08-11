a476a8d148c9b4b6b0170044ab1432cbc0775bcf
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-08-11 00:17:04 -0700

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{" ", "brightness_icon.sh $(ls /sys/class/backlight)",                5,              0},
+	{"", "mic.sh",								5,              0},
 	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
         {"", "wifi_icon.sh",							5,              0},
         {"", "vol_icon.sh",							5,              0},
